fc15240ee6eef959f4ecb819daeb8d4593237773
dhonatas <dhonatas12alves@gmail.com>
2026-03-21 17:15:49 -0300

    add Procfile and update server.js to use dynamic port

--- src/server.js
+++ src/server.js
@@ -3,13 +3,15 @@ require('dotenv/config')
 const { buildApp } = require('./app')
 const { prisma } = require('./lib/prisma')
 
+const port = Number(process.env.PORT || 3333)
+
 async function start() {
   const app = buildApp()
 
   try {
     await prisma.$connect()
-    await app.listen({ port: 3333, host: '0.0.0.0' })
-    console.log('Servidor rodando na porta 3333')
+    await app.listen({ port, host: '0.0.0.0' })
+    console.log(`Servidor rodando na porta ${port}`)
   } catch (error) {
     app.log.error(error)
     process.exit(1)
